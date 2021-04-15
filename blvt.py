c63d65305323546fcf69b2561ebc8df26e172609
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:24:43 +0700

    Add placeholders for Binance API BLVT requests

---
+++ blvt.py
@@ -0,0 +1,22 @@
+def get_leverage_token_info():
+    return
+
+
+def subscribe_blvt():
+    return
+
+
+def query_subscription_record():
+    return
+
+
+def redeem_blvt():
+    return
+
+
+def query_redemption_record():
+    return
+
+
+def get_blvt_user_limit_info():
+    return
