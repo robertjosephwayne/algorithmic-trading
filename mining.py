2a6d185a2d733cf6a244d86326995d3265321539
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:48:27 +0700

    Add placeholders for Binance API mining requests

---
+++ mining.py
@@ -0,0 +1,52 @@
+# Mining
+
+def acquiring_algorithm():
+    return
+
+
+def acquiring_coin_name():
+    return
+
+
+def request_for_detail_miner_list():
+    return
+
+
+def request_for_miner_list():
+    return
+
+
+def revenue_list():
+    return
+
+
+def statistic_list():
+    return
+
+
+def account_list():
+    return
+
+
+def earnings_list():
+    return
+
+
+def extra_bonus_list():
+    return
+
+
+def hash_rate_resale_list():
+    return
+
+
+def hash_rate_resale_details():
+    return
+
+
+def hash_rate_resale_request():
+    return
+
+
+def cancel_hash_rate_resale_configuration():
+    return
