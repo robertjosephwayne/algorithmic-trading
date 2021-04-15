27739fa324e802495dab286e839e1f388e5c08cc
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:51:39 +0700

    Add placeholders for Binance API savings requests

---
+++ savings.py
@@ -0,0 +1,56 @@
+# Savings
+
+def get_flexible_product_list():
+    return
+
+
+def get_left_daily_purchase_quota_of_flexible_product():
+    return
+
+
+def purchase_flexible_product():
+    return
+
+
+def get_left_daily_redemption_quota_of_flexible_product():
+    return
+
+
+def redeem_flexible_product():
+    return
+
+
+def get_flexible_product_position():
+    return
+
+
+def get_fixed_and_customized_fixed_project():
+    return
+
+
+def purchase_customized_fixed_project():
+    return
+
+
+def get_customized_fixed_project_position():
+    return
+
+
+def lending_account():
+    return
+
+
+def get_purchase_record():
+    return
+
+
+def get_redemption_record():
+    return
+
+
+def get_interest_history():
+    return
+
+
+def change_fixed_activity_position_to_daily_position():
+    return
