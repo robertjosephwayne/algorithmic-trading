beb9ef37290161b53cd6973803ab44a69a5e3c7e
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:53:31 +0700

    Add placeholders for Binance API trade requests

---
+++ trade.py
@@ -0,0 +1,56 @@
+# Trade
+
+def test_new_order():
+    return
+
+
+def new_order():
+    return
+
+
+def cancel_order():
+    return
+
+
+def cancel_all_open_orders_on_symbol():
+    return
+
+
+def query_order():
+    return
+
+
+def current_open_orders():
+    return
+
+
+def all_orders():
+    return
+
+
+def new_oco():
+    return
+
+
+def cancel_oco():
+    return
+
+
+def query_oco():
+    return
+
+
+def query_all_oco():
+    return
+
+
+def query_open_oco():
+    return
+
+
+def account_info():
+    return
+
+
+def account_trade_list():
+    return
