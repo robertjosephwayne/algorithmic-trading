544d2616051e2dc9c79657cab847f773d70533e6
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:43:05 +0700

    Add placeholders for Binance API margin requests

---
+++ margin.py
@@ -0,0 +1,130 @@
+# Margin - Isolated
+
+def create_isolated_margin_account():
+    return
+
+
+def isolated_margin_account_transfer():
+    return
+
+
+def get_isolated_margin_account_transfer_history():
+    return
+
+
+def query_isolated_margin_account_info():
+    return
+
+
+def query_isolated_margin_pair():
+    return
+
+
+def get_all_isolated_margin_pairs():
+    return
+
+# TODO: Margin
+
+def margin_account_transfer():
+    return
+
+
+def margin_account_borrow():
+    return
+
+
+def margin_account_repay():
+    return
+
+
+def query_margin_asset():
+    return
+
+
+def query_margin_pair():
+    return
+
+
+def get_all_margin_assets():
+    return
+
+
+def get_all_margin_pairs():
+    return
+
+
+def query_margin_price_index():
+    return
+
+
+def margin_account_new_order():
+    return
+
+
+def margin_account_cancel_order():
+    return
+
+
+def margin_account_cancel_all_open_orders_for_symbol():
+    return
+
+
+def get_transfer_history():
+    return
+
+
+def query_loan_record():
+    return
+
+
+def query_repay_record():
+    return
+
+
+def get_interest_history():
+    return
+
+
+def get_force_liquidation_record():
+    return
+
+
+def query_margin_account_details():
+    return
+
+
+def query_margin_account_order():
+    return
+
+
+def query_margin_account_open_order():
+    return
+
+
+def query_margin_account_all_order():
+    return
+
+
+def query_margin_account_trade_list():
+    return
+
+
+def query_max_borrow():
+    return
+
+
+def query_max_transfer_out_amount():
+    return
+
+
+def toggle_bnb_burn_status():
+    return
+
+
+def get_bnb_burn_status():
+    return
+
+
+def query_margin_interest_rate_history():
+    return
+
