b85493286b94b8abc11db2e784dc211293a1803d
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:25:24 +0700

    Add placeholders for Binance API data stream requests

---
+++ data_stream.py
@@ -0,0 +1,40 @@
+# Spot
+
+def create_new_listen_key_spot():
+    return
+
+
+def renew_listen_key_spot():
+    return
+
+
+def delete_listen_key_spot():
+    return
+
+
+# Margin - Isolated
+
+def create_new_isolated_margin_account_listen_key():
+    return
+
+
+def renew_isolated_margin_account_listen_key():
+    return
+
+
+def delete_isolated_margin_account_listen_key():
+    return
+
+
+# Margin - Cross
+
+def create_new_listen_key_cross():
+    return
+
+
+def renew_listen_key_cross():
+    return
+
+
+def delete_listen_key_cross():
+    return
