8facc7b2cdf46ed477e2199835225fc6d89ef8f1
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:25:00 +0700

    Add placeholders for Binance API BSWAP requests

---
+++ bswap.py
@@ -0,0 +1,30 @@
+def list_all_swap_pools():
+    return
+
+
+def get_pool_liquidity_info():
+    return
+
+
+def add_liquidity():
+    return
+
+
+def remove_liquidity():
+    return
+
+
+def get_liquidity_operation_record():
+    return
+
+
+def request_quote():
+    return
+
+
+def swap():
+    return
+
+
+def get_swap_history():
+    return
