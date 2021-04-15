e275220a1647a204b5e365ca925207d3c9418274
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 15:00:34 +0700

    Add placeholders for Binance API corporate requests

---
+++ corporate.py
@@ -0,0 +1,90 @@
+# Corporate
+
+def create_virtual_sub_account():
+    return
+
+
+def query_sub_account_list():
+    return
+
+
+def query_sub_account_list_sapi():
+    return
+
+
+def query_sub_account_spot_asset_transfer_history():
+    return
+
+
+def sub_account_spot_asset_transfer():
+    return
+
+
+def query_sub_account_futures_asset_transfer_history():
+    return
+
+def sub_account_futures_asset_transfer():
+    return
+
+
+def query_sub_account_assets():
+    return
+
+
+def query_sub_account_assets_sapi():
+    return
+
+
+def query_sub_account_spot_assets_summary():
+    return
+
+
+def get_sub_account_deposit_address():
+    return
+
+
+def get_sub_account_deposit_history():
+    return
+
+
+def get_sub_account_status_margin_futures():
+    return
+
+
+def enable_margin_for_sub_account():
+    return
+
+
+def get_detail_on_sub_account_margin_account():
+    return
+
+
+def get_summary_on_sub_account_margin_account():
+    return
+
+
+def enable_futures_for_sub_account():
+    return
+
+
+def get_detail_on_sub_account_futures_account():
+    return
+
+
+def get_summary_on_sub_account_futures_account():
+    return
+
+# TODO: Finish adding placeholders for remaining corporate requests
+
+# Corporate v2
+
+def get_detail_sub_account_futures_account_v2():
+    return
+
+
+def get_summary_sub_account_futures_account_v2():
+    return
+
+
+def get_futures_position_risk_sub_account_v2():
+    return
