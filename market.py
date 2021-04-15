69bccbdad42d2f03ef6cb7640a146afbe02fe9cc
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:46:18 +0700

    Add placeholders for Binance API market requests

---
+++ market.py
@@ -0,0 +1,48 @@
+# Market
+
+def test_connectivity():
+    return
+
+
+def check_server_time():
+    return
+
+
+def exchange_info():
+    return
+
+
+def order_book():
+    return
+
+
+def recent_trades_list():
+    return
+
+
+def old_trade_lookup():
+    return
+
+
+def compressed_aggregate_trades_list():
+    return
+
+
+def kline_candlestick_data():
+    return
+
+
+def current_average_price():
+    return
+
+
+def twenty_four_hour_ticker_price_change_statistics():
+    return
+
+
+def symbol_price_ticker():
+    return
+
+
+def symbol_order_book_ticker():
+    return
