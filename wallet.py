222dc6502e3c149580590d30f5de1bf7ccae1ecd
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:27:49 +0700

    Add placeholders for Binance API wallet requests

---
+++ wallet.py
@@ -0,0 +1,108 @@
+def system_status():
+    return
+
+
+def system_status_sapi():
+    return
+
+
+def all_coins_info():
+    return
+
+
+def daily_account_snapshot():
+    return
+
+
+def disable_fast_withdraw_switch():
+    return
+
+
+def enable_fast_withdraw_switch():
+    return
+
+
+def withdraw_sapi():
+    return
+
+
+def withdraw():
+    return
+
+
+def deposit_history_supporting_network():
+    return
+
+
+def deposit_history():
+    return
+
+
+def withdraw_history_supporting_network():
+    return
+
+
+def withdraw_history():
+    return
+
+
+def deposit_address_supporting_network():
+    return
+
+
+def deposit_address():
+    return
+
+
+def account_status():
+    return
+
+
+def account_status_sapi():
+    return
+
+
+def account_api_trading_status():
+    return
+
+
+def account_api_trading_status_sapi():
+    return
+
+
+def dust_log():
+    return
+
+
+def dust_log_sapi():
+    return
+
+
+def trade_fee():
+    return
+
+
+def trade_fee_sapi():
+    return
+
+
+def asset_detail():
+    return
+
+
+def asset_detail_sapi():
+    return
+
+def dust_transfer():
+    return
+
+def asset_dividend_record():
+    return
+
+
+def user_universal_transfer():
+    return
+
+
+def query_user_universal_transfer_history():
+    return
