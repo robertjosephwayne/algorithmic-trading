31352a4f01584d427f7f20fe8598ffb5c588a1a3
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-04-15 14:25:41 +0700

    Add placeholders for Binance API futures requests

---
+++ futures.py
@@ -0,0 +1,90 @@
+def new_future_account_transfer():
+    return
+
+
+def get_future_account_transaction_history_list():
+    return
+
+
+def borrow_for_cross_collateral():
+    return
+
+
+def cross_collateral_borrow_history():
+    return
+
+
+def repay_for_cross_collateral():
+    return
+
+
+def cross_collateral_repayment_history():
+    return
+
+
+def cross_collateral_wallet():
+    return
+
+
+def cross_collateral_wallet_v2():
+    return
+
+
+def cross_collateral_info():
+    return
+
+
+def cross_collateral_info_v2():
+    return
+
+
+def calculate_rate_after_adjust_cross_collateral_ltv():
+    return
+
+
+def calculate_rate_after_adjust_cross_collateral_ltv_v2():
+    return
+
+
+def get_max_amount_for_adjust_cross_collateral_ltv():
+    return
+
+
+def get_max_amount_for_adjust_cross_collateral_ltv_v2():
+    return
+
+
+def adjust_cross_collateral_ltv():
+    return
+
+
+def adjust_cross_collateral_ltv_v2():
+    return
+
+
+def adjust_cross_collateral_ltv_history():
+    return
+
+
+def cross_collateral_liquidation_history():
+    return
+
+
+def check_collateral_repay_limit():
+    return
+
+
+def get_collateral_repay_quote():
+    return
+
+
+def repay_with_collateral():
+    return
+
+
+def collateral_repayment_result():
+    return
+
+
+def cross_collateral_interest_history():
+    return
