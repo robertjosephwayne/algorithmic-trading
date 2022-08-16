d97ae5f7ebd427df20c71432e7c1f2998dc22829
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-08-16 16:10:39 +0700

    Add placeholders for Coinbase API transfers requests

---
+++ coinbase/transfers.py
@@ -0,0 +1,37 @@
+# Transfers
+
+def deposit_from_coinbase_account():
+    return
+
+
+def deposit_from_payment_method():
+    return
+
+
+def get_all_payment_methods():
+    return
+
+
+def get_all_transfers():
+    return
+
+
+def get_transfer():
+    return
+
+
+def withdraw_to_coinbase_account():
+    return
+
+
+def withdraw_to_crypto_address():
+    return
+
+
+def get_fee_estimate_for_crypto_withdrawal():
+    return
+
+
+def withdraw_to_payment_method():
+    return
+
