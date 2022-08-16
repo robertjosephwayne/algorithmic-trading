e2e1176884765a1f86340c1898cdf7f9c94323a3
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-08-16 16:12:11 +0700

    Add placeholders for Coinbase API orders requests

---
+++ coinbase/orders.py
@@ -0,0 +1,25 @@
+# Orders
+
+def get_all_fills():
+    return
+
+
+def get_all_orders():
+    return
+
+
+def cancel_all_orders():
+    return
+
+
+def create_new_order():
+    return
+
+
+def get_order():
+    return
+
+
+def cancel_order():
+    return
+
