71356ce7bc8e77ff6d81d91634de0519273ac644
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-08-16 16:14:48 +0700

    Add placeholders for Coinbase API Coinbase products requests

---
+++ coinbase/products.py
@@ -0,0 +1,29 @@
+# Products
+
+def get_all_known_trading_pairs():
+    return
+
+
+def get_single_product():
+    return
+
+
+def get_product_book():
+    return
+
+
+def get_product_candles():
+    return
+
+
+def get_product_stats():
+    return
+
+
+def get_product_ticker():
+    return
+
+
+def get_product_trades():
+    return
+
