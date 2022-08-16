5e357fbdb4f64ca76a6f2abaa275c7c9c5a7dfe7
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-08-16 16:16:37 +0700

    Add placeholders for Coinbase API Coinbase profiles requests

---
+++ coinbase/profiles.py
@@ -0,0 +1,25 @@
+# Profiles
+
+def get_profiles():
+    return
+
+
+def create_profile():
+    return
+
+
+def transfer_funds_between_profiles():
+    return
+
+
+def get_profile():
+    return
+
+
+def rename_profile():
+    return
+
+
+def delete_profile():
+    return
+
