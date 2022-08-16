90a900de498400c8933864b25983dabd4c2e8db2
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-08-16 17:01:42 +0700

    Create blueprint for coinbase/accounts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Blueprint, Flask
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,7 +8,14 @@ app = Flask(__name__)
 
 name = os.environ.get("NAME")
 
+coinbase = Blueprint('coinbase', __name__, url_prefix='/coinbase')
+accounts = Blueprint('accounts', __name__, url_prefix='/accounts')
+
+coinbase.register_blueprint(accounts)
+app.register_blueprint(coinbase)
+
 @app.route('/')
 def hello_world():
+    print(app.url_map)
     return 'Hello, World!'
 
