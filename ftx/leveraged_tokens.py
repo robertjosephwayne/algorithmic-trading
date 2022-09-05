5ab213be6f92c6f43b9e2718f2856d0300e40255
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 21:55:38 +0700

    Add placeholders for FTX API - Leveraged Tokens

---
+++ ftx/leveraged_tokens.py
@@ -0,0 +1,33 @@
+# Leveraged Tokens
+
+def list_leveraged_tokens():
+    return
+
+
+def get_token_info():
+    return
+
+
+def get_leveraged_token_balances():
+    return
+
+
+def list_leveraged_token_creation_requests():
+    return
+
+
+def request_leveraged_token_creation():
+    return
+
+
+def list_leveraged_token_redemption_requests():
+    return
+
+
+def request_leveraged_token_redemption():
+    return
+
+
+def request_etf_rebalance_info():
+    return
+    
