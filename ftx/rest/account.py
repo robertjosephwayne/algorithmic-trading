4e537028171049d1ab2faca19ae0c13960b41b5b
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 23:51:00 +0700

    Add comments with endpoints for FTX REST API - Account

--- ftx/rest/account.py
+++ ftx/rest/account.py
@@ -1,24 +1,30 @@
 # Account
 
+# GET /account
 def get_account_information():
     return
 
 
+# POST /historical_balances/requests
 def request_historical_balances_and_positions_snapshot():
     return
 
 
+# GET /historical_balances/requests/<request_id>
 def get_historical_balances_and_positions_snapshot():
     return
 
 
+# GET /historical_balances/requests
 def get_all_historical_balances_and_positions_snapshot():
     return
 
 
+# GET /positions
 def get_positions():
     return
 
 
+# POST /account/leverage
 def change_account_leverage():
     return
