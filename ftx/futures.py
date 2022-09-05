4aa11cf9fd44bf367d18ebdf655bde4f53800b67
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 21:35:34 +0700

    Add placeholders for FTX API - Futures

---
+++ ftx/futures.py
@@ -0,0 +1,33 @@
+# Futures
+
+def list_all_futures():
+    return
+
+
+def get_future():
+    return
+
+
+def get_future_stats():
+    return
+
+
+def get_funding_rates():
+    return
+
+
+def get_index_weights():
+    return
+
+
+def get_expired_futures():
+    return
+
+
+def get_historical_index():
+    return
+
+
+def get_index_constituents():
+    return
+
