6f5c2896f781eecf66d7092327681dda199df869
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 21:34:05 +0700

    Add placeholders for FTX API - Markets

---
+++ ftx/markets.py
@@ -0,0 +1,21 @@
+# Markets
+
+def get_markets():
+    return
+
+
+def get_single_market():
+    return
+
+
+def get_orderbook():
+    return
+
+
+def get_trades():
+    return
+
+
+def get_historical_prices():
+    return
+
