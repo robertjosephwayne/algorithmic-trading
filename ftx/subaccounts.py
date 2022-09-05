36098c796e300b4dac3d2cfd67ab09d3cb6d8782
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 21:31:14 +0700

    Add placeholders for FTX API - Subaccounts

---
+++ ftx/subaccounts.py
@@ -0,0 +1,27 @@
+# Subaccounts
+
+def get_all_subaccounts():
+    return
+
+
+def create_subaccount():
+    return
+
+
+def change_subaccount_name():
+    return
+
+
+def delete_subaccount():
+    return
+
+
+def get_subaccount_balances():
+    return
+
+
+def transfer_between_subaccounts():
+    return
+
+
+
