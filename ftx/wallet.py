08c338ed523266aa7928ab3b53b7c1c73444525e
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 21:43:36 +0700

    Add placeholders for FTX API - Wallet

---
+++ ftx/wallet.py
@@ -0,0 +1,68 @@
+# Wallet
+
+def get_coins():
+    return
+
+
+def get_balances():
+    return
+
+
+def get_balances_of_all_accounts():
+    return
+
+
+def get_deposit_address():
+    return
+
+
+def get_deposit_address_list():
+    return
+
+
+def get_deposit_history():
+    return
+
+
+def get_withdrawal_history():
+    return
+
+
+def request_withdrawal():
+    return
+
+
+def get_airdrops():
+    return
+
+
+def get_withdrawal_fees():
+    return
+
+
+def get_saved_addresses():
+    return
+
+
+def create_saved_addresses():
+    return
+
+
+def delete_saved_addresses():
+    return
+
+
+def register_sen_deposit():
+    return
+
+
+def request_sen_withdrawal():
+    return
+
+
+def register_signet_deposit():
+    return
+
+
+def request_signet_withdrawal():
+    return
