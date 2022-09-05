f95388c6bb8d78e118e37b9b05fa02312d299c75
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-06 00:13:32 +0700

    Add comments with endpoints for FTX REST API - Markets

--- ftx/rest/markets.py
+++ ftx/rest/markets.py
@@ -1,21 +1,26 @@
 # Markets
 
+# GET /markets
 def get_markets():
     return
 
 
+# GET /markets/{market_name}
 def get_single_market():
     return
 
 
+# GET /markets/{market_name}/orderbook?depth={depth}
 def get_orderbook():
     return
 
 
+# GET /markets/{market_name}/trades
 def get_trades():
     return
 
 
+# GET /markets/{market_name}/candles?resolution={resolution}&start_time={start_time}&end_time={end_time}
 def get_historical_prices():
     return
 
