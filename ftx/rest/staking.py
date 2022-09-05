6f1371e534ae12506c30075a8d79ea543181e847
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-06 00:28:24 +0700

    Add comments with endpoints for FTX REST API - Staking

--- ftx/rest/staking.py
+++ ftx/rest/staking.py
@@ -1,25 +1,36 @@
 # Staking
 
+# GET /staking/stakes
 def get_stakes():
     return
 
 
-def unstake_request():
+# GET /staking/unstake_requests
+def get_unstake_requests():
     return
 
 
+# GET /staking/balances
 def get_stake_balances():
     return
 
 
+# POST /staking/unstake_requests
+def unstake_request():
+    return
+
+
+# DELETE /staking/unstake_requests/{request_id}
 def cancel_unstake_request():
     return
 
 
+# GET /staking/staking_rewards
 def get_staking_rewards():
     return
 
 
+# POST /srm_stakes/stakes
 def stake_request():
     return
 
