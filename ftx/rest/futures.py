a3522f38a771bc85f874192cd5af71b37637c6d5
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 23:54:40 +0700

    Add comments with endpoints for FTX REST API - Futures

--- ftx/rest/futures.py
+++ ftx/rest/futures.py
@@ -1,33 +1,41 @@
 # Futures
 
+# GET /futures
 def list_all_futures():
     return
 
 
+# GET /futures/{future_name}
 def get_future():
     return
 
 
+# GET /futures/{future_name}/stats
 def get_future_stats():
     return
 
 
+# GET /funding_rates
 def get_funding_rates():
     return
 
 
+# GET /indexes/{index_name}/weights
 def get_index_weights():
     return
 
 
+# GET /expired_futures
 def get_expired_futures():
     return
 
 
+# GET /indexes/{market_name}/candles?resolution={resolution}&start_time={start_time}&end_time={end_time}
 def get_historical_index():
     return
 
 
+# GET /index_constituents/{underlying}
 def get_index_constituents():
     return
 
