abfdf7d1cf8cd28a5d70e321467865bf6d751dd7
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 23:08:56 +0700

    Remove Coinbase blueprints

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ load_dotenv()
 app = Flask(__name__)
 
 
-name = os.environ.get("NAME")
-
-coinbase = Blueprint('coinbase', __name__, url_prefix='/coinbase')
-accounts = Blueprint('accounts', __name__, url_prefix='/accounts')
-
-coinbase.register_blueprint(accounts)
-app.register_blueprint(coinbase)
-
 @app.route('/')
 def hello_world():
     print(app.url_map)
