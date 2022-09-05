5377e23d2bc19b08c863faff2dd160f98a6fa083
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 22:11:57 +0700

    Add placeholders for FTX API - NFTs

---
+++ ftx/nfts.py
@@ -0,0 +1,86 @@
+# NFTs
+
+def list_nfts():
+    return
+
+
+def get_nft_info():
+    return
+
+
+def get_nft_trades():
+    return
+
+
+def get_all_nft_trades():
+    return
+
+
+def get_nft_account_info():
+    return
+
+
+def get_all_nft_collections():
+    return
+
+
+def get_nft_balances():
+    return
+
+
+def make_nft_offer():
+    return
+
+
+def buy_nft():
+    return
+
+
+def create_auction():
+    return
+
+
+def edit_auction():
+    return
+
+
+def cancel_auction():
+    return
+
+
+def get_bids():
+    return
+
+
+def place_bid():
+    return
+
+
+def get_nft_deposits():
+    return
+
+
+def get_nft_withdrawals():
+    return
+
+
+def get_nft_fills():
+    return
+
+
+def redeem_nft():
+    return
+
+
+def get_nft_gallery():
+    return
+
+
+def get_gallery_settings():
+    return
+
+
+def edit_gallery_settings():
+    return
+
+    
