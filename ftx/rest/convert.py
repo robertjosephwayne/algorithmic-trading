89fec433bba368ccd396c30efb30a9181553b2ec
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 23:52:08 +0700

    Add comments with endpoints for FTX REST API - Convert

--- ftx/rest/convert.py
+++ ftx/rest/convert.py
@@ -1,13 +1,16 @@
 # Convert
 
+# POST /otc/quotes
 def request_quote():
     return
 
 
+# GET /otc/quotes/{quoteId}
 def get_quote_status():
     return
 
 
+# POST /otc/quotes/{quote_id}/accept
 def accept_quote():
     return
 
