719d1080acf023f17330a2ecbf538284657b7a50
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 23:10:39 +0700

    Create FtxClient with initial methods to access rest API endpoints

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 from dotenv import load_dotenv
-from flask import Blueprint, Flask
+from flask import Flask
+from ftx.rest.client import FtxClient
 
 load_dotenv()
 app = Flask(__name__)
 
+api_key = os.environ.get("API_KEY")
+api_secret = os.environ.get("API_SECRET")
 
 @app.route('/')
 def hello_world():
-    print(app.url_map)
+    ftx = FtxClient(api_key, api_secret)
     return 'Hello, World!'
 
