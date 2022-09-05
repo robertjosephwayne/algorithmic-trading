0f1ccfd01d505527e6739fe57eddbd7b9b4d7a25
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 22:07:07 +0700

    Add placeholders for FTX API - Spot Margin

---
+++ ftx/spot_margin.py
@@ -0,0 +1,41 @@
+# Spot Margin
+
+def get_lending_history():
+    return
+
+
+def get_borrow_rates():
+    return
+
+
+def get_lending_rates():
+    return
+
+
+def get_daily_borrowed_amounts():
+    return
+
+
+def get_market_info():
+    return
+
+
+def get_my_borrow_history():
+    return
+
+
+def get_my_lending_history():
+    return
+
+
+def get_lending_offers():
+    return
+
+
+def get_lending_info():
+    return
+
+
+def submit_lending_offer():
+    return
+
