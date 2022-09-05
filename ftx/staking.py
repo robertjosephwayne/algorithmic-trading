9ef6af8dd25c3310c2c85e2e47ab508ef56398be
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 22:04:31 +0700

    Add placeholders for FTX API - Staking

---
+++ ftx/staking.py
@@ -0,0 +1,25 @@
+# Staking
+
+def get_stakes():
+    return
+
+
+def unstake_request():
+    return
+
+
+def get_stake_balances():
+    return
+
+
+def cancel_unstake_request():
+    return
+
+
+def get_staking_rewards():
+    return
+
+
+def stake_request():
+    return
+
