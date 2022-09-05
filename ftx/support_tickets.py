d828860e41a14709a10e9adadf1b753aa8214816
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 22:16:26 +0700

    Add placeholders for FTX API - Support Tickets

---
+++ ftx/support_tickets.py
@@ -0,0 +1,25 @@
+# Support Tickets
+
+def get_all_support_tickets():
+    return
+
+
+def get_support_ticket_messages():
+    return
+
+
+def send_support_message():
+    return
+
+
+def update_support_ticket_status():
+    return
+
+
+def count_total_number_of_unread_support_messages():
+    return
+
+
+def mark_support_messages_read():
+    return
+
