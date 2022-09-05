8f3dc62bf33975fa30d3ddab52e36ffcbf7cfeb3
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 21:48:05 +0700

    Add placeholders for FTX API - Orders

---
+++ ftx/orders.py
@@ -0,0 +1,89 @@
+# Orders
+
+def get_open_orders():
+    return
+
+
+def get_order_history():
+    return
+
+
+def get_open_trigger_orders():
+    return
+
+
+def get_trigger_order_triggers():
+    return
+
+
+def get_trigger_order_history():
+    return
+
+
+def get_twap_orders():
+    return
+
+
+def get_twap_order_executions():
+    return
+
+
+def place_order():
+    return
+
+
+def place_trigger_order():
+    return
+
+
+def place_twap_order():
+    return
+
+
+def modify_order():
+    return
+
+
+def modify_order_by_client_id():
+    return
+
+
+def modify_trigger_order():
+    return
+
+
+def get_order_status():
+    return
+
+
+def get_order_status_by_client_id():
+    return
+
+
+def cancel_order():
+    return
+
+
+def cancel_twap_order():
+    return
+
+
+def cancel_order_by_client_id():
+    return
+
+
+def cancel_open_trigger_order():
+    return
+
+
+def cancel_all_orders():
+    return
+
+
+def bulk_cancel_orders():
+    return
+
+
+def bulk_cancel_orders_by_client_id():
+    return
+
