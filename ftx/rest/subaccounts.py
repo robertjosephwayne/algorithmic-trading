68e338d988d035fc76e05f3159c1ab5413523efa
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-06 00:29:41 +0700

    Add comments with endpoints for FTX REST API - Subaccounts

--- ftx/rest/subaccounts.py
+++ ftx/rest/subaccounts.py
@@ -1,25 +1,31 @@
 # Subaccounts
 
+# GET /subaccounts
 def get_all_subaccounts():
     return
 
 
+# POST /subaccounts
 def create_subaccount():
     return
 
 
+# POST /subaccounts/update_name
 def change_subaccount_name():
     return
 
 
+# DELETE /subaccounts
 def delete_subaccount():
     return
 
 
+# GET /subaccounts/{nickname}/balances
 def get_subaccount_balances():
     return
 
 
+# POST /subaccounts/transfer
 def transfer_between_subaccounts():
     return
 
