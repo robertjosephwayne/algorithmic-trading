3a8890763117821fd6d9966cf4bef9e976ee6b92
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 21:37:41 +0700

    Add placeholders for FTX API - Account

---
+++ ftx/account.py
@@ -0,0 +1,24 @@
+# Account
+
+def get_account_information():
+    return
+
+
+def request_historical_balances_and_positions_snapshot():
+    return
+
+
+def get_historical_balances_and_positions_snapshot():
+    return
+
+
+def get_all_historical_balances_and_positions_snapshot():
+    return
+
+
+def get_positions():
+    return
+
+
+def change_account_leverage():
+    return
