48cd6f95d316084536be8f38cd05424ccfae1e2e
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-05 22:02:54 +0700

    Add placeholders for FTX API - Options

---
+++ ftx/options.py
@@ -0,0 +1,61 @@
+# Options
+
+def list_quote_requests():
+    return
+
+
+def your_quote_requests():
+    return
+
+
+def create_quote_request():
+    return
+
+
+def cancel_quote_request():
+    return
+
+
+def get_quotes_for_your_quote_request():
+    return
+
+
+def create_quote():
+    return
+
+
+def get_my_quotes():
+    return
+
+
+def cancel_quote():
+    return
+
+
+def accept_options_quote():
+    return
+
+
+def get_account_options_info():
+    return
+
+
+def get_options_positions():
+    return
+
+
+def get_public_options_trades():
+    return
+
+
+def get_options_fills():
+    return
+
+
+def get_24h_option_volume():
+    return
+
+
+def get_option_open_interest():
+    return
+
