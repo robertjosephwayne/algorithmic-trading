c1894a4f09ace988d71112ec47e4a70da354fd44
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-11 17:39:06 +0700

    Rename hello_world() function to main()

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 api_key = os.environ.get("API_KEY")
 api_secret = os.environ.get("API_SECRET")
 
-@app.route('/')
-def hello_world():
-    ftx = FtxClient(api_key, api_secret)
-    return 'Hello, World!'
 
+@app.route('/')
+def main():
+    ftx = FtxClient(api_key, api_secret)
+    balances = ftx.get_balances()
+    print(balances)
+    return 'Success'
