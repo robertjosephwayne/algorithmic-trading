9bd4b4fa6140f223ac2fd2a2229f9c5974adb241
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-09-11 22:11:23 +0700

    Remove Flask endpoints, create basic logger

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+import pandas as pd
 from ftx.rest.client import FtxClient
+import tkinter as tk
+import logging
 
-load_dotenv()
-app = Flask(__name__)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
 api_key = os.environ.get("API_KEY")
 api_secret = os.environ.get("API_SECRET")
 
-
-@app.route('/')
-def main():
-    return 'Success'
-
-
-@app.route('/test')
-def test():
+if __name__ == '__main__':
+    load_dotenv()
+    root = tk.Tk()
+    root.mainloop()
     ftx = FtxClient(api_key, api_secret)
-    balances = ftx.get_balances()
-    print(balances)
-    return 'Success'
+
